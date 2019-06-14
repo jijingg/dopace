23358c038c421dfa59c5c5f1cc004f0d4eb84644
goco_2018pc <goco.v@163.com>
2019-06-14 11:11:52 +0800

    hydra finished

--- init.el
+++ init.el
@@ -1,10 +1,12 @@
 (require 'package)
 (package-initialize)
 
+(setq emacs-d-root (concat (getenv "HOME") "/.emacs.d/"))
+
 (let ((file-name-handler-alist nil))
   ;; If config is pre-compiled, then load that
-  (if (file-exists-p (expand-file-name "dopace.elc" user-emacs-directory))
-      (load-file (expand-file-name "dopace.elc" user-emacs-directory))
+  (if (file-exists-p (expand-file-name "dopace.elc" emacs-d-root))
+      (load-file (expand-file-name "dopace.elc" emacs-d-root))
     ;; Otherwise use org-babel to tangle and load the configuration
     (require 'org)
-    (org-babel-load-file (expand-file-name "dopace.org" user-emacs-directory))))
+    (org-babel-load-file (expand-file-name "dopace.org" emacs-d-root))))
